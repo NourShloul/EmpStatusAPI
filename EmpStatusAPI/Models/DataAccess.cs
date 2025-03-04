f9e1919558c9f71b9497bb175728412b4eba4435
NourShloul <shloulnour31@gmail.com>
2025-03-04 15:29:16 +0300

    Update DataAccess.cs

--- EmpStatusAPI/Models/DataAccess.cs
+++ EmpStatusAPI/Models/DataAccess.cs
@@ -8,14 +8,12 @@ namespace EmpStatusAPI.Models
     public class DataAccess
     {
         private EmpDBEntities db = new EmpDBEntities();
-
-        // جلب المستخدم بناءً على الرقم الوطني
+        
         public User GetUserByNAT(int nat)
         {
             return db.Users.FirstOrDefault(u => u.NationalNumber == nat);
         }
 
-        // جلب جميع الرواتب لموظف معين
         public List<int> GetSalariesByUserId(int userId)
         {
             return db.Salaries.Where(s => s.UserID == userId).Select(s => s.SalaryAmount).ToList();
